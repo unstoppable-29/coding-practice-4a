cfe8bba0afdfa5a2dd017a2f9358b7959368ad41
unstoppable-29 <johnwilsonmitta9912@gmail.com>
2023-03-16 12:39:13 +0000

    superoverleague

---
+++ index.js
@@ -0,0 +1,10 @@
+const element = (
+  //   Write your code here.
+  <div>
+    <h1 className="name">Super Over League</h1>
+    <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+    <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
